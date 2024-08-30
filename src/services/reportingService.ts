fb43b19790dbffd85e6d47be3b6d170c473cb97d
hhassan01 <hassanhamza0101@gmail.com>
2024-08-30 13:05:25 +0500

    transaction event process fixed and deploymen

--- src/services/reportingService.ts
+++ src/services/reportingService.ts
@@ -1,6 +1,7 @@
 import { Model } from 'mongoose'
+
 import { IStatement } from '../models/statement'
-import { TransactionEvent, TransactionType } from '../events/transactionEvent'
+import { TransactionEvent } from '../events/transactionEvent'
 import { getMonthYearFromDate } from '../utils/dateUtils'
 import { NotFoundException } from '../utils/exceptions'
 
@@ -14,46 +15,8 @@ export class ReportingService {
   async processTransactionEvent(event: TransactionEvent): Promise<void> {
     const { accountId, currency, amount, type, date } = event
     const { month, year } = getMonthYearFromDate(date)
-
-    const statement = await this.getOrCreateStatement(accountId, month, year)
-    const currencyStatement = this.getOrCreateCurrencyStatement(
-      statement,
-      currency,
-    )
-
-    const currencyStatementIndex = statement.currencies.findIndex(
-      (cs) => cs.currency === currency,
-    )
-    this.updateCurrencyStatement(currencyStatement, amount, type, date)
-    statement.currencies[currencyStatementIndex] = currencyStatement
-
-    await statement.save()
-  }
-
-  async getMonthlyStatements(
-    accountId: string,
-    year: number,
-    month: number,
-  ): Promise<IStatement> {
-    const statement = await this.statementModel
-      .findOne({ accountId, year, month })
-      .exec()
-
-    if (!statement) {
-      throw new NotFoundException('Statement not found')
-    }
-
-    return statement
-  }
-
-  private async getOrCreateStatement(
-    accountId: string,
-    month: number,
-    year: number,
-  ): Promise<IStatement> {
-    let statement = await this.statementModel
-      .findOne({ accountId, month, year })
-      .exec()
+    
+    let statement = await this.statementModel.findOne({ accountId, month, year }).exec()
     if (!statement) {
       statement = new this.statementModel({
         accountId,
@@ -62,13 +25,7 @@ export class ReportingService {
         currencies: [],
       })
     }
-    return statement
-  }
-
-  private getOrCreateCurrencyStatement(
-    statement: IStatement,
-    currency: string,
-  ) {
+    
     let currencyStatement = statement.currencies.find(
       (cs) => cs.currency === currency,
     )
@@ -80,23 +37,36 @@ export class ReportingService {
       }
       statement.currencies.push(currencyStatement)
     }
-    return currencyStatement
-  }
-
-  private updateCurrencyStatement(
-    currencyStatement: IStatement['currencies'][0],
-    amount: number,
-    type: TransactionType,
-    date: Date,
-  ) {
+    
+    const currencyStatementIndex = statement.currencies.findIndex(
+      (cs) => cs.currency === currency,
+    )
     const previousClosingBalance = currencyStatement.closingBalance || 0
 
-    if (type === TransactionType.INBOUND) {
+    if (type === 'INBOUND') {
       currencyStatement.closingBalance = previousClosingBalance + amount
-    } else if (type === TransactionType.OUTBOUND) {
+    } else if (type === 'OUTBOUND') {
       currencyStatement.closingBalance = previousClosingBalance - amount
     }
 
     currencyStatement.transactions.push({ type, amount, date })
+
+    statement.currencies[currencyStatementIndex] = currencyStatement
+
+    await statement.save()
+  }
+
+  async getMonthlyStatements(
+    accountId: string,
+    year: number,
+    month: number,
+  ): Promise<IStatement> {    
+    const statement = await this.statementModel.findOne({ accountId, year, month }).exec()
+
+    if(!statement) {
+      throw new NotFoundException('Statement not found')
+    }
+
+    return statement
   }
 }
